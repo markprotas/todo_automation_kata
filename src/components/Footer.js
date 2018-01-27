a557327dc26526b58a3501fb28e06554b55d4f9e
Mark Protas <mprotas@pillartechnology.com>
2018-01-27 10:58:19 -0500

    Added automation ids to filter links in the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,18 @@ import FilterLink from "../containers/FilterLink";
 
 const Footer = () => (
   <p>
-    Show: <FilterLink filter="SHOW_ALL">All</FilterLink>
+    Show:{" "}
+    <FilterLink filter="SHOW_ALL" linkId="showAll">
+      All
+    </FilterLink>
     {", "}
-    <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>
+    <FilterLink filter="SHOW_ACTIVE" linkId="showActive">
+      Active
+    </FilterLink>
     {", "}
-    <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>
+    <FilterLink filter="SHOW_COMPLETED" linkId="showCompleted">
+      Completed
+    </FilterLink>
   </p>
 );
 
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Link = ({ active, children, onClick }) => {
+const Link = ({ active, children, onClick, linkId }) => {
   if (active) {
     return <span>{children}</span>;
   }
 
   return (
     <a
+      id={linkId}
       href=""
       onClick={e => {
         e.preventDefault();
@@ -22,7 +23,8 @@ const Link = ({ active, children, onClick }) => {
 Link.propTypes = {
   active: PropTypes.bool.isRequired,
   children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired
+  onClick: PropTypes.func.isRequired,
+  linkId: PropTypes.string.isRequired
 };
 
 export default Link;
